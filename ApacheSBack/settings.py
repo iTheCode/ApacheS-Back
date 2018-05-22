19741ae6d316b3910d364765d8b4ef9469c301e4
iTheCode <ithecode.ica@gmail.com>
2018-05-22 02:16:47 -0500

    test fix cors

--- ApacheSBack/settings.py
+++ ApacheSBack/settings.py
@@ -148,14 +148,14 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 AUTH_USER_MODEL = 'ApacheS.User'
 
 CORS_ORIGIN_ALLOW_ALL = True 
-CORS_ORIGIN_WHITELIST = (
-    '*'
-)
-CORS_ALLOW_METHODS = (
-    'DELETE',
-    'GET',
-    'OPTIONS',
-    'PATCH',
-    'POST',
-    'PUT',
+CORS_ALLOW_HEADERS = (
+    'accept',
+    'accept-encoding',
+    'authorization',
+    'content-type',
+    'dnt',
+    'origin',
+    'user-agent',
+    'x-csrftoken',
+    'x-requested-with',
 )
